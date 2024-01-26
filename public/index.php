388245f8da0684ae377d1decb0af67724234463f
ErvinAtVoco <ervin.tombak@voco.ee>
2024-01-26 02:58:47 +0200

    Added DI and containers

--- public/index.php
+++ public/index.php
@@ -2,12 +2,22 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$container = new Container();
+
+AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello slim!");
+    return $response;
+});
+
+$app->get('/hello', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello slim!");
     return $response;
 });
